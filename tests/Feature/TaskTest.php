703880faa4fb698afec49225dd87710b2274df0a
kalidusachintha <kalidusachintha7@gmail.com>
2025-04-03 12:30:04 +0530

    Add feature testing

---
+++ tests/Feature/TaskTest.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Enums\TaskStatus;
+use App\Models\Task;
+use Tests\TestCase;
+
+class TaskTest extends TestCase
+{
+    /**
+     * Get all the tasks
+     */
+    public function test_get_all_tasks(): void
+    {
+        Task::factory()->count(15)->create();
+        $response = $this->getJson('/api/v1/tasks');
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(15, 'data');
+    }
+
+    /**
+     * Can create task
+     * @return void
+     */
+    public function test_can_create_task(): void
+    {
+        $response = $this->postJson('/api/v1/tasks', [
+            'title' => 'Test Task',
+            'description' => 'Test Description',
+            'status' => TaskStatus::PENDING->value,
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJsonFragment(['title' => 'Test Task']);
+    }
+
+    /**
+     * Can get a task
+     * @return void
+     */
+    public function test_can_get_single_task(): void
+    {
+        $task = Task::factory()->create([
+            'status' => TaskStatus::PENDING->value,
+        ]);
+
+        $response = $this->getJson("/api/v1/tasks/{$task->id}");
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    'id' => $task->id,
+                    'title' => $task->title,
+                    'description' => $task->description,
+                    'status' => TaskStatus::PENDING->value,
+                    'created_date' => $task->created_at->format('Y-m-d'),
+                ]
+            ]);
+    }
+
+    /**
+     * Can update the task
+     * @return void
+     */
+    public function test_can_update_task(): void
+    {
+        $task = Task::factory()->create();
+
+        $updatedData = [
+            'title' => 'Updated Task',
+            'description' => 'This task has been updated',
+            'status' => TaskStatus::PENDING->value,
+        ];
+
+        $response = $this->putJson("/api/v1/tasks/{$task->id}", $updatedData);
+
+        $this->assertNotEquals($task->title, $updatedData['title']);
+        $response->assertStatus(200)
+            ->assertJson([
+                'message' => 'Task updated successfully'
+            ]);
+    }
+
+    /**
+     * Can delete a task
+     * @return void
+     */
+    public function test_can_delete_task(): void
+    {
+        $task = Task::factory()->create();
+
+        $response = $this->deleteJson("/api/v1/tasks/{$task->id}");
+        $response->assertStatus(200);
+        $this->assertSoftDeleted('tasks', ['id' => $task->id]);
+    }
+
+    /**
+     * Create a task without title
+     * @return void
+     */
+    public function test_cannot_create_task_without_title(): void
+    {
+        $response = $this->postJson('/api/v1/tasks', [
+            'description' => 'Description without title',
+            'status' => TaskStatus::PENDING->value,
+        ]);
+
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors(['title']);
+    }
+}
